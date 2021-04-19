ca419b1dcde0fa0c8e8cb173a51a9625f8a0bb26
donissousa <donisgtr@gmail.com>
2021-04-19 13:46:28 -0300

    primeira aula

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({
+        menssage: "Ola nlw 05",
+    });
+});
+
+app.post("/users", (request, response) => {
+    return response.json({ 
+        message: "Usuario salvo com sucesso!"
+    });
+});
+
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
